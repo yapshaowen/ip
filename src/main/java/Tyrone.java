1d9d6da34465f01774bb89fb9a3df2b39df6dcf3
Shao Wen <shaokey2004@gmail.com>
2026-01-15 16:13:03 +0800

    Rename main class

--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Tyrone {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +6,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
     }
 }
