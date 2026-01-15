35b0d972c3661a4918572ad7a300ad77997da9d3
Shao Wen <shaokey2004@gmail.com>
2026-01-15 16:30:51 +0800

    Add greeting

--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -1,11 +1,13 @@
 public class Tyrone {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                      " _______   ______   ___  _   _ _____ \n"
+                    + "|_   _\\ \\ / /  _ \\ / _ \\| \\ | | ____|\n"
+                    + "  | |  \\ V /| |_) | | | |  \\| |  _|  \n"
+                    + "  | |   | | |  _ <| |_| | |\\  | |___ \n"
+                    + "  |_|   |_| |_| \\_\\\\___/|_| \\_|_____|\n";
+        System.out.println("Wassup dawg? It's ya boy\n" + logo);
+        System.out.println("Bye dawg!\n");
 
     }
 }
