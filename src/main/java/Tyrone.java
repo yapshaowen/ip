4ccf84bf355186036db3971772395dd47019cc13
Shao Wen <shaokey2004@gmail.com>
2026-01-16 14:35:08 +0800

    Add support for add

--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -2,9 +2,13 @@ import java.util.*;
 
 public class Tyrone {
     private static final String LINE = "____________________________________________________________\n";
-    //private static final String LINE = "_".repeat(50) + "\n";
+    private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
         String logo =
                       " _______   ______   ___  _   _ _____ \n"
                     + "|_   _\\ \\ / /  _ \\ / _ \\| \\ | | ____|\n"
@@ -16,9 +20,9 @@ public class Tyrone {
         System.out.println("Watchu want?\n");
         printLine();
 
-        Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
                 printLine();
                 System.out.println("Bye dawg!\n");
@@ -26,8 +30,20 @@ public class Tyrone {
                 break;
             }
 
+            /*if (input.equals("list")) {
+                printLine();
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                printLine();
+                continue;
+            }*/
+
+            tasks[taskCount] = input;
+            taskCount++;
+
             printLine();
-            System.out.println(input);
+            System.out.println("added: " + input);
             printLine();
         }
 
