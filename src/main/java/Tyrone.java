5387ff7c85cc9764589550fd9ca1fc61e4ce1405
Shao Wen <shaokey2004@gmail.com>
2026-01-18 15:37:25 +0800

    Add support for adding tasks of different types

--- src/main/java/Tyrone.java
+++ src/main/java/Tyrone.java
@@ -64,6 +64,43 @@ public class Tyrone {
                 continue;
             }
 
+            if (input.startsWith("todo ")) {
+                String desc = input.substring(5).trim();
+                taskCount = addTask(tasks, taskCount, new Todo(desc));
+                continue;
+            }
+
+            if (input.startsWith("deadline ")) {
+                int byPos = input.indexOf(" /by ");
+                if (byPos == -1) {
+                    printLine();
+                    System.out.println("Invalid deadline bro! You gotta use: deadline <task> /by <time>");
+                    printLine();
+                    continue;
+                }
+                String desc = input.substring(9, byPos).trim();
+                String by = input.substring(byPos + 5).trim();
+                taskCount = addTask(tasks, taskCount, new Deadline(desc, by));
+                continue;
+            }
+
+            if (input.startsWith("event ")) {
+                int fromPos = input.indexOf(" /from ");
+                int toPos = input.indexOf(" /to ");
+                if (fromPos == -1 || toPos == -1 || toPos < fromPos) {
+                    printLine();
+                    System.out.println("Invalid format man! You gotta use: event <task> /from <start> /to <end>");
+                    printLine();
+                    continue;
+                }
+
+                String desc = input.substring(6, fromPos).trim();
+                String from = input.substring(fromPos + 7, toPos).trim();
+                String to = input.substring(toPos + 5).trim();
+                taskCount = addTask(tasks, taskCount, new Event(desc, from, to));
+                continue;
+            }
+
             tasks[taskCount] = new Task(input);
             taskCount++;
 
@@ -73,6 +110,26 @@ public class Tyrone {
         }
 
         sc.close();
+    }
+
+    private static int addTask(Task[] tasks, int taskCount, Task task) {
+        if (taskCount >= MAX_TASKS) {
+            printLine();
+            System.out.println("My bad bro, I can only store up to " + MAX_TASKS + " tasks my man");
+            printLine();
+            return taskCount;
+        }
+
+        tasks[taskCount] = task;
+        taskCount++;
+
+        printLine();
+        System.out.println("Gotchu my guy. I added this task:");
+        System.out.println(" " + task);
+        System.out.println("Now you got " + taskCount + " tasks in yo list");
+        printLine();
+
+        return taskCount;
     }
 
     private static void printLine() {
