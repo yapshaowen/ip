7efacb0e59bc5d0ce475a3f325543c8a5e6b2f03
Shao Wen <shaokey2004@gmail.com>
2026-01-28 20:51:35 +0800

    Refactor code to be more oop

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public Task remove(int index) {
+        return tasks.remove(index);
+    }
+
+    public void mark(int index) {
+        tasks.get(index).mark();
+    }
+
+    public void unmark(int index) {
+        tasks.get(index).unmark();
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+}
